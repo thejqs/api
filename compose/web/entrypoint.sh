c3949c14ec817b0909adc07a397fb718684dad61
Jacob Sanders <j.q.sanders@gmail.com>
2022-07-09 17:43:24 -0400

    update docker-compose things

--- compose/web/entrypoint.sh
+++ compose/web/entrypoint.sh
@@ -7,14 +7,14 @@ if [ -z "$POSTGRES_USER" ]; then
     export POSTGRES_USER=postgres
 fi
 
-export DATABASE_URL=${DATABASE_URL:-postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER}
+export DATABASE_URL=${DATABASE_URL:-db://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER}
 
 function postgres_ready(){
 python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="db")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
